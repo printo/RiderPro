a31a222b288b207e6429d5ff09fa045c3c4d0c8b
Mervin-Abraham <mervinabey@gmail.com>
2025-10-17 06:47:50 +0000

    Fix API entry point for ES modules
    - Convert api/index.js from CommonJS to ES modules
    - Use import/export instead of require/module.exports
    - This should fix the Vercel serverless function execution

--- api/index.js
+++ api/index.js
@@ -1,8 +1,5 @@
-const express = require('express');
-const path = require('path');
-
 // Import your Express app from the built server
-const app = require('../dist/index.js');
+import app from '../dist/index.js';
 
 // Export the Express app for Vercel
-module.exports = app;
+export default app;
