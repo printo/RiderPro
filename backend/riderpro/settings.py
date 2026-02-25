eb375f3f25a0f01f18b779bb31a24d8d5171ff8a
Kanna Perumal <kannangates@gmail.com>
2026-02-26 02:08:35 +0530

    quick fix

--- backend/riderpro/settings.py
+++ backend/riderpro/settings.py
@@ -42,7 +42,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'rest_framework.middleware.AuthenticationMiddleware',
 ]
 
 ROOT_URLCONF = 'riderpro.urls'
